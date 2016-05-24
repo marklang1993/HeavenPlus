b59af646db005dccdb65f3d994d7b6097e776718
Chen Lang <marklang1993@gmail.com>
2016-05-24 21:37:36 +0900

    Begin

---
+++ HeavenPlus/HeavenPlus/MainStage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeavenPlus
+{
+    public partial class MainStage : Form
+    {
+        public MainStage()
+        {
+            InitializeComponent();
+        }
+    }
+}
